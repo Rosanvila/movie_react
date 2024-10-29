3b68b94ed11ff7c0410eb7f47c4e6f6030f97a9f
Robin Sanchez-Avila <robin.sa28@gmail.com>
2024-10-29 10:58:09 +0100

    Initialisation projet

---
+++ src/components/SearchForm.js
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+const SearchForm = () => {
+    const [movie, setMovie] = useState('');
+
+    useEffect(() => {
+        axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=03a005ba73a452bea154f766bc1e7562`)
+            .then(response => {
+                console.log(response.data.results);
+                setMovie(response.data.results);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    } , []);
+
+    return (
+        <div className={"form-component"}>
+            <div className={"form-container"}>
+                <form>
+                    <input type={"text"} placeholder={"Select a movie"}/>
+                    <input type={"submit"} value={"Search"} />
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default SearchForm;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "../components/Header";
+import SearchForm from "../components/SearchForm";
+
+const Home = () => {
+    return (
+        <div>
+            <Header/>
+            <SearchForm/>
+        </div>
+    );
+};
+
+export default Home;
