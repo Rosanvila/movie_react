cca4cc5e1ee84b0ad229e5113d4b7c57c30d89bc
Robin Sanchez-Avila <robin.sa28@gmail.com>
2024-10-30 16:30:49 +0100

    [fix] Refactor App component structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import Fav from './pages/Fav';
-import './styles/index.scss';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Fav from "./pages/Fav";
+import "./styles/index.scss";
 function App() {
-    return (
-        <Router>
-            <div>
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/fav" element={<Fav/>}/>
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/fav" element={<Fav />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
