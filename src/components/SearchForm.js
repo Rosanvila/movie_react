510bcb094a07c25556b9c6a090d1b8ae022d668d
Robin Sanchez-Avila <robin.sa28@gmail.com>
2024-10-30 14:37:57 +0100

    Add initial styles and components for home and user list pages

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,30 +1,38 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const SearchForm = () => {
-    const [movie, setMovie] = useState('');
+  const [movie, setMovie] = useState("");
 
-    useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=03a005ba73a452bea154f766bc1e7562`)
-            .then(response => {
-                console.log(response.data.results);
-                setMovie(response.data.results);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    } , []);
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=03a005ba73a452bea154f766bc1e7562`
+      )
+      .then((response) => {
+        console.log(response.data.results);
+        setMovie(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    return (
-        <div className={"form-component"}>
-            <div className={"form-container"}>
-                <form>
-                    <input type={"text"} placeholder={"Select a movie"}/>
-                    <input type={"submit"} value={"Search"} />
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className={"form-component"}>
+      <div className={"form-container"}>
+        <form>
+          <input type={"text"} placeholder={"Select a movie"} />
+          <input type={"submit"} value={"Search"} />
+        </form>
+      </div>
+      <div className="result">
+        {movie.slice(0, 12).map((movie) => (
+          <h3>{movie.title}</h3>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SearchForm;
