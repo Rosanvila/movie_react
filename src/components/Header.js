dd2d9232299ed48a0bc862561ef16aa451df5c3b
Robin Sanchez-Avila <robin.sa28@gmail.com>
2024-10-31 12:09:24 +0100

    modification

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,32 @@
-import React from 'react';
-import {NavLink} from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <header>
-            <h1>Movies</h1>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink to="/" className={nav => nav.isActive ? "nav-active" : ""}>Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/fav" className={nav => nav.isActive ? "nav-active" : ""}>Coups de coeur</NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header>
+      <h1>Movies</h1>
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={(nav) => (nav.isActive ? "nav-active" : "")}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/fav"
+              className={(nav) => (nav.isActive ? "nav-active" : "")}
+            >
+              Coups de coeur
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
